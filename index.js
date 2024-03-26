99e10e475d16fd7d74646a74b290127318ba6e06
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2024-03-25 21:56:56 -0300

    include typeof validation

--- index.js
+++ index.js
@@ -1,59 +1,72 @@
-require('dotenv').config();
-const venom = require('venom-bot');
-const axios = require('axios');
-const wifi = require('node-wifi');
+require("dotenv").config();
+const venom = require("venom-bot");
+const axios = require("axios");
+const wifi = require("node-wifi");
 
 wifi.init({ iface: null });
 
 // Connect to network using .env variables
-wifi.connect({ ssid: process.env.WIFI_SSID, password: process.env.WIFI_PASSWORD }, error => {
-  if (error) console.error(error);
-  else console.log('Connected to WiFi');
-});
+wifi.connect(
+  { ssid: process.env.WIFI_SSID, password: process.env.WIFI_PASSWORD },
+  (error) => {
+    if (error) console.error(error);
+    else console.log("Connected to WiFi");
+  }
+);
 
-venom.create({ session: 'session-name' })
-.then(client => start(client))
-.catch(error => console.error(error));
+venom
+  .create({ session: "session-name" })
+  .then((client) => start(client))
+  .catch((error) => console.error(error));
 
 async function start(client) {
   client.onMessage(async (message) => {
-    const formattedMessage = message.body.toLowerCase().trim(); // Format the message to lowercase and remove extra spaces
+    if (typeof message.body === "string") {
+      const formattedMessage = message.body.toLowerCase().trim();
 
-    try {
-      if (formattedMessage === 'open gate') {
-        await sendRawSignal(message.from, client);
-      } else if (formattedMessage === 'open file') {
-        await sendFileSignal(message.from, client);
-      } else if (formattedMessage.startsWith('jammer')) {
-        await toggleJammer(formattedMessage, message.from, client);
+      try {
+        if (formattedMessage === "open gate") {
+          await sendRawSignal(message.from, client);
+        } else if (formattedMessage === "open file") {
+          await sendFileSignal(message.from, client);
+        } else if (formattedMessage.startsWith("jammer")) {
+          await toggleJammer(formattedMessage, message.from, client);
+        }
+      } catch (error) {
+        console.error("Error: ", error);
+        await client.sendText(
+          message.from,
+          "An error occurred, please try again."
+        );
       }
-    } catch (error) {
-      console.error('Error: ', error);
-      await client.sendText(message.from, 'An error occurred, please try again.');
     }
   });
 }
 
 async function sendRawSignal(to, client) {
-  const rawResponse = await axios.get(`${process.env.DEVICE_URL}/SENDRAW?P1=${process.env.RAW_SIGNAL}`);
-  await client.sendText(to, '(SEND RAW SIGNAL) Opening gate, please wait...');
-  console.log('Response RAW:', rawResponse.data);
+  const rawResponse = await axios.get(
+    `${process.env.DEVICE_URL}/SENDRAW?P1=${process.env.RAW_SIGNAL}`
+  );
+  await client.sendText(to, "(SEND RAW SIGNAL) Opening gate, please wait...");
+  console.log("Response RAW:", rawResponse.data);
 }
 
 async function sendFileSignal(to, client) {
   await axios.get(`${process.env.DEVICE_URL}/TXSD`);
-  const fileResponse = await axios.get(`${process.env.DEVICE_URL}/APPLY?P1=CURRENT&P2=vale_do_cedro-gate.sub`);
-  await client.sendText(to, '(SEND FILE SIGNAL) Opening gate, please wait...');
-  console.log('Response File:', fileResponse.data);
+  const fileResponse = await axios.get(
+    `${process.env.DEVICE_URL}/APPLY?P1=CURRENT&P2=vale_do_cedro-gate.sub`
+  );
+  await client.sendText(to, "(SEND FILE SIGNAL) Opening gate, please wait...");
+  console.log("Response File:", fileResponse.data);
 }
 
 async function toggleJammer(command, to, client) {
-  const mode = command.split(' ')[1];
-  if (mode === 'true') {
+  const mode = command.split(" ")[1];
+  if (mode === "true") {
     await setupJammer(true, client, to);
-  } else if (mode === 'false') {
+  } else if (mode === "false") {
     await axios.get(`${process.env.DEVICE_URL}/APPLY?P1=JAMMER&P2=false`);
-    await client.sendText(to, 'Turning off Jammer');
+    await client.sendText(to, "Turning off Jammer");
   }
 }
 
